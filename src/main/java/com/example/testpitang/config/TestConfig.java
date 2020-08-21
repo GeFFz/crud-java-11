a6ccde786f5c444f67567826fbc636a6edd7026d
Geffz <gefferson_oliveira@outlook.com>
2020-08-21 15:38:12 -0300

    Implementando Entidade Payment, relação Product-Payment, correção de JsonIgnore em Order, Metodos SubTotal e Total.

--- src/main/java/com/example/testpitang/config/TestConfig.java
+++ src/main/java/com/example/testpitang/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.testpitang.entities.Category;
 import com.example.testpitang.entities.Order;
 import com.example.testpitang.entities.OrderItem;
+import com.example.testpitang.entities.Payment;
 import com.example.testpitang.entities.Product;
 import com.example.testpitang.entities.User;
 import com.example.testpitang.entities.enums.OrderStatus;
@@ -80,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi3 = new OrderItem(o2, p3, 4, p3.getPrice());
 		OrderItem oi4 = new OrderItem(o3, p5, 7, p5.getPrice()); 
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2020-08-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 		
 	}
